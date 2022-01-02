d26dd69ed0d5f0071b07403d07a09a0328119490
Quang <quangnguyenminh3152@gmail.com>
2022-01-02 12:53:46 +0700

    reformat code

--- MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/fragments/FragmenDetailArtist.java
+++ MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/fragments/FragmenDetailArtist.java
@@ -61,6 +61,6 @@ public class FragmenDetailArtist extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmen_detail_artist, container, false);
+        return inflater.inflate(R.layout.fragment_detail_artist, container, false);
     }
 }
