2f40dab77b1cecbb0c623fcddb93e64b2d6e1022
Quang <quangnguyenminh3152@gmail.com>
2022-01-02 21:23:47 +0700

    add play song activity

---
+++ MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/activities/PlaySongActivity.java
@@ -0,0 +1,17 @@
+package com.example.musiccuoiky.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.musiccuoiky.R;
+
+public class PlaySongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+    }
+
+}
