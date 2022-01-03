cb6d15a85e391c6c8a594484a93bb12b876f7765
TaAnhQuan <77721385+TaAnhQuan@users.noreply.github.com>
2022-01-03 22:11:47 +0700

    fragment detail update

--- MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/fragments/FragmentDetailArtist.java
+++ MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/fragments/FragmentDetailArtist.java
@@ -1,5 +1,6 @@
 package com.example.musiccuoiky.fragments;
 
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,7 @@ import com.example.musiccuoiky.adapters.AdapterSongForArtist;
 import com.example.musiccuoiky.models.Album;
 import com.example.musiccuoiky.models.Song;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -68,16 +71,37 @@ public class FragmentDetailArtist extends Fragment {
         //recycle view part
         rcvAlbumForArtist.setAdapter(adapterAlbum);
         rcvSongForArtist.setAdapter(adapterSongForArtist);
-        layoutManagerAlbum.setAutoMeasureEnabled(true);
-        layoutManagerSong.setAutoMeasureEnabled(true);
+        //layoutManagerAlbum.setAutoMeasureEnabled(true);
+        //layoutManagerSong.setAutoMeasureEnabled(true);
         rcvSongForArtist.setNestedScrollingEnabled(false);
         rcvAlbumForArtist.setNestedScrollingEnabled(false);
 
         return view;
     }
 
-    private List<Album> getListAlbum(String string) {
-        return null;
+    private List<Album> getListAlbum(String artistID) {
+        List<Album> list = new ArrayList<>();
+        String[] m_data = {MediaStore.Audio.Albums._ID,
+                MediaStore.Audio.Albums.ALBUM,
+                MediaStore.Audio.Albums.ALBUM_ART,
+                MediaStore.Audio.Albums.ARTIST,
+                MediaStore.Audio.Albums.NUMBER_OF_SONGS,
+        };
+        Cursor c = getContext().getContentResolver()
+                .query(MediaStore.Audio.Artists.Albums.getContentUri("external", Long.parseLong(artistID)),
+                        m_data, null, null, null);
+
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            String id, album, album_art, artist, number_of_songs;
+            id = c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Albums._ID));
+            album = c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM));
+            album_art = c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM_ART));
+            artist = c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Albums.ARTIST));
+            number_of_songs = " ("+c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Albums.NUMBER_OF_SONGS))+")";
+            Album albums = new Album(id, album, album_art, artist, number_of_songs);
+            list.add(albums);
+        }
+        return list;
     }
 
     private List<Song> getListSong(String artistID) {
