b1605acf0e114312e79c3c551e476f7f9f0c1ede
Quang <quangnguyenminh3152@gmail.com>
2022-01-03 12:45:14 +0700

    update method

--- MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/activities/MainActivity.java
+++ MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/activities/MainActivity.java
@@ -3,14 +3,54 @@ package com.example.musiccuoiky.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.musiccuoiky.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void updateList(){
+
+    }
+
+    public void setControl(){
+
+    }
+
+    public static void addFragmentDetailAlbum(Bundle bundle){
+
+    }
+
+    public static void addFragmentDetailArtist(Bundle bundle){
+
+    }
+
+    public static void addFragmentDetailPlaylist(Bundle bundle){
+
+    }
+
+    public static void updateUI(){
+
+    }
+
+    void addPermission() {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
