8d6a31760bc55a997bf501bac6adf74bebafabce
Quang <quangnguyenminh3152@gmail.com>
2022-01-03 22:13:22 +0700

    add dialog

---
+++ MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/CreatePlaylistDialog.java
@@ -0,0 +1,32 @@
+package com.example.musiccuoiky;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+public class CreatePlaylistDialog extends DialogFragment {
+    public static CreatePlaylistDialog instance;
+    public CreatePlaylistDialog(){
+        instance = this;
+    }
+
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new MaterialDialog.Builder(instance.getContext())
+                .title("Tạo danh sách")
+                .inputRange(1,50)
+                .positiveText("Tạo")
+                .negativeText("Thoát")
+                .input("", "Danh sách phát...", new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(MaterialDialog dialog, CharSequence input) {
+                        // Do something
+                        long playistId = MusicPlayer.createPlaylist(getActivity(), input.toString());
+                        updatePlaylist();
+                    }
+                }).show();
+    }
+
+}
