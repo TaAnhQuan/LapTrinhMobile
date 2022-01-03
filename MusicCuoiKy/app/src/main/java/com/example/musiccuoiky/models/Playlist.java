a7bce05a92256b9aa48dbe06a06e3c7d7e0d8a13
TaAnhQuan <77721385+TaAnhQuan@users.noreply.github.com>
2022-01-03 11:08:27 +0700

    model commit update

--- MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/models/Playlist.java
+++ MusicCuoiKy/app/src/main/java/com/example/musiccuoiky/models/Playlist.java
@@ -1,4 +1,37 @@
 package com.example.musiccuoiky.models;
 
 public class Playlist {
+    private long id;
+    private String name;
+    private int songCount;
+
+    public Playlist(long id, String name, int songCount) {
+        this.id = id;
+        this.name = name;
+        this.songCount = songCount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
 }
